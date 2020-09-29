814028c89b0eb2e8be16b8ae679b9f606f028dbe
cori schlegel <cori@glitch.com>
2020-09-29 08:49:52 -0500

    some efficiencies

--- timelapse.sh
+++ timelapse.sh
@@ -6,19 +6,21 @@
 #
 # Can also record using webcam via the imagesnap program.
 #
+
+output_folder="$HOME/OneDrive/Archive/timelapse_output"
 while true; do
     timestamp=$(date '+%Y-%m-%dT%H%M%S');
     echo "Capturing screen at $timestamp."
     # For one screen:
-    screencapture -t jpg -x "output/screen/screen1-$timestamp.jpg";
+    screencapture -t jpg -x "$output_folder/screen/screen1-$timestamp.jpg";
     # For two screens:
-    # screencapture -x "screen1-$timestamp.png" "screen2-$timestamp.png";
+    # screencapture -x "$output_folder/screen/screen1-$timestamp.png" "$output_folder/screen/screen2-$timestamp.png";
 
     # If you have a webcam, you might want to capture that too.
     echo "Capturing isight at $timestamp."
-    ./imagesnap/imagesnap -q "output/isight/isight-$timestamp.jpg";
+    imagesnap -q "$output_folder/webcam/webcam-$timestamp.jpg";
     # sleep 60;
-    sleep 120;
+    sleep 300;	#	5 mins
 done;
 
 # After it's finished, you probably want to play around with image
